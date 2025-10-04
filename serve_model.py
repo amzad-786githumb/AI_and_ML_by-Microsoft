6a2a495c839c9ae6b7227b2f3a0ee638c44771e8
amzad-786githumb <73783489+amzad-786githumb@users.noreply.github.com>
2025-10-04 11:56:01 +0530

    Add files via upload

---
+++ serve_model.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+#naming the flask app
+app = Flask(__name__)
+
+# Load the model
+model = joblib.load('/workspaces/AI_and_ML_by-Microsoft/8_Preparing_a_model_for_deployment/iris_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict(np.array(data['input']).reshape(1, -1))
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
